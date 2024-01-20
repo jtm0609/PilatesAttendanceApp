1002c5d869788a132229fbd6d6fdcf4a84cb5479
Takmin <jtm0609@naver.com>
2024-01-20 11:12:14 +0900

    [feat] 관리자 UI 생성 및 NavigationComponent 적용

---
+++ presentation/src/main/java/com/example/cmong_pilates_attendance_project/view/admin/AdminActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cmong_pilates_attendance_project.view.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cmong_pilates_attendance_project.R
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
---
+++ presentation/src/main/java/com/example/cmong_pilates_attendance_project/view/admin/ChangeAttendanceCountFragment.kt
@@ -0,0 +1,21 @@
+package com.example.cmong_pilates_attendance_project.view.admin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cmong_pilates_attendance_project.R
+
+
+class ChangeAttendanceCountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_attendance_count, container, false)
+    }
+
+}
