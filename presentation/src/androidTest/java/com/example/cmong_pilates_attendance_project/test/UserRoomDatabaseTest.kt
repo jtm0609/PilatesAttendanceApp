13d1bd0a0a28c2203ada320b8a9cbe26b7d1fa53
bella-hyejin <bellal9388@gmail.com>
2024-02-17 21:12:52 +0900

    [Test] 사용자 수정 기능 테스트 완료

--- presentation/src/androidTest/java/com/example/cmong_pilates_attendance_project/test/UserRoomDatabaseTest.kt
+++ presentation/src/androidTest/java/com/example/cmong_pilates_attendance_project/test/UserRoomDatabaseTest.kt
@@ -2,26 +2,35 @@ package com.example.cmong_pilates_attendance_project.test
 
 import android.util.Log
 import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry
 import com.example.data.data.UserEntity
 import com.example.data.db.UserDao
 import com.example.data.db.UserDatabase
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import java.io.IOException
 import java.util.Calendar
 
 class UserRoomDatabaseTest {
     private lateinit var userDao: UserDao
+    private lateinit var roomdb: UserDatabase
     // Context를 가져오는 것 때문에 위치를 변경하였습니다.
     
     @Before
     fun setup() {
-        val roomdb = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
+        roomdb = Room.inMemoryDatabaseBuilder(
+            InstrumentationRegistry.getInstrumentation().targetContext,
             UserDatabase::class.java
-        ).allowMainThreadQueries().build()
+        ).build()
 
         userDao = roomdb.userDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        roomdb.close()
     }
 
     @Test
@@ -49,24 +58,26 @@ class UserRoomDatabaseTest {
             startDateTime = Calendar.getInstance().timeInMillis,
             endDateTime = Calendar.getInstance().timeInMillis + 1000,
         )
+
         userDao.insertUser(beforeData).test().assertComplete()
 
+        val user = userDao.selectUsers().blockingGet().first()
 
-        val data = UserEntity(
-            name = "홍길동2",
-            phoneNumber = "01012341234",
-            duration = "4주",
-            startDateTime = Calendar.getInstance().timeInMillis,
-            endDateTime = Calendar.getInstance().timeInMillis + 1000,
-        )
-        userDao.updateUser(data).test().assertComplete()
-
-        val test = userDao.selectUsers().test()
+        user.apply {
+            name = "홍길동2"
+            phoneNumber = "01012341234"
+            duration = "4주"
+            startDateTime = Calendar.getInstance().timeInMillis
+            endDateTime = Calendar.getInstance().timeInMillis + 1000
+        }
+        
+        userDao.updateUser(user).test().assertComplete()
 
         val result = userDao.selectUsers().blockingGet()
         Log.d("data", "result => ${result[0].name}")
         Log.d("data", "result => ${result[0].phoneNumber}")
 
 //        test.assertValue { it[0].name == data.name }
+        userDao.selectUsers().test().assertValue { it[0].name == user.name }
     }
 }
