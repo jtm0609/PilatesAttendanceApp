007dae647227d9061abd6d0c7d164145b4e4789d
Takmin <jtm0609@naver.com>
2025-04-23 22:08:15 +0900

    [refactor] Hilt 관련 로직 수정

--- data-local/src/main/java/com/example/local/di/DataSourceModule.kt
+++ data-local/src/main/java/com/example/local/di/DataSourceModule.kt
@@ -4,26 +4,21 @@ import com.example.data.local.AdminDataSource
 import com.example.data.local.UserDataSource
 import com.example.local.impl.AdminDataSourceImpl
 import com.example.local.impl.UserDataSourceImpl
-import com.example.local.room.dao.AdminDao
-import com.example.local.room.dao.UserDao
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataSourceModule {
-    @Singleton
-    @Provides
-    fun provideLocalDataSource(userDao: UserDao): UserDataSource {
-        return UserDataSourceImpl(userDao)
-    }
+abstract class DataSourceModule {
 
     @Singleton
-    @Provides
-    fun provideAdminLocalDataSource(adminDao: AdminDao): AdminDataSource {
-        return AdminDataSourceImpl(adminDao)
-    }
+    @Binds
+    abstract fun bindLocalDataSource(userDataSourceImpl: UserDataSourceImpl) : UserDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindAdminLocalDataSource(adminDataSourceImpl: AdminDataSourceImpl): AdminDataSource
 }
--- data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -4,30 +4,21 @@ import com.example.domain.repository.AdminRepository
 import com.example.data.impl.AdminRepositoryImpl
 import com.example.domain.repository.UserRepository
 import com.example.data.impl.UserRepositoryImpl
-import com.example.data.local.AdminDataSource
-import com.example.data.local.UserDataSource
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-    @Singleton
-    @Provides
-    fun provideUserRepository(
-        userDataSource: UserDataSource
-    ): UserRepository {
-        return UserRepositoryImpl(userDataSource)
-    }
+abstract class RepositoryModule {
 
     @Singleton
-    @Provides
-    fun provideAdminRepository(
-        adminDataSource: AdminDataSource
-    ): AdminRepository {
-        return AdminRepositoryImpl(adminDataSource)
-    }
+    @Binds
+    abstract fun bindUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindAdminRepository(adminRepositoryImpl: AdminRepositoryImpl): AdminRepository
 }
